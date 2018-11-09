9cb9dae6667331cb659047f59a6d85eda08d9c03
Dmytro Nosan <dimanosan@gmail.com>
2018-11-09 14:07:05 +0200

    fix and polish tests

--- embedded-cassandra-parent/embedded-cassandra/src/test/java/com/github/nosan/embedded/cassandra/local/AbstractLocalCassandraTests.java
+++ embedded-cassandra-parent/embedded-cassandra/src/test/java/com/github/nosan/embedded/cassandra/local/AbstractLocalCassandraTests.java
@@ -21,6 +21,7 @@ import java.nio.file.Paths;
 import java.util.Objects;
 import java.util.UUID;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -43,11 +44,14 @@ import com.github.nosan.embedded.cassandra.cql.StaticCqlScript;
 import com.github.nosan.embedded.cassandra.test.support.CaptureOutput;
 import com.github.nosan.embedded.cassandra.test.support.CauseMatcher;
 import com.github.nosan.embedded.cassandra.util.FileUtils;
+import com.github.nosan.embedded.cassandra.util.OS;
+import com.github.nosan.embedded.cassandra.util.PortUtils;
+import com.github.nosan.embedded.cassandra.util.StringUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Abstract test-class for {@link LocalProcess}.
+ * Abstract test-class for {@link LocalCassandra}.
  *
  * @author Dmytro Nosan
  */
@@ -69,14 +73,16 @@ public abstract class AbstractLocalCassandraTests {
 	@Test
 	public void shouldStartedIfTransportDisabled() {
 		this.factory.setConfigurationFile(getClass().getResource("/cassandra-transport.yaml"));
-		new CassandraRunner(this.factory).run();
+		new CassandraRunner(this.factory).run(assertPort(Settings::getStoragePort));
 	}
 
 	@Test
 	public void shouldOverrideJavaHome() {
+		if (!OS.isWindows()) {
+			this.throwable.expectCause(new CauseMatcher(IOException.class,
+					"Unable to find java executable"));
+		}
 		this.throwable.expect(CassandraException.class);
-		this.throwable.expectCause(new CauseMatcher(IOException.class,
-				"Unable to find java executable"));
 		this.factory.setJavaHome(Paths.get(UUID.randomUUID().toString()));
 		new CassandraRunner(this.factory).run();
 	}
@@ -84,8 +90,7 @@ public abstract class AbstractLocalCassandraTests {
 	@Test
 	public void shouldStartStopOnlyOnce() {
 		CassandraRunner runner = new CassandraRunner(this.factory);
-		CqlAssert cqlAssert = new CqlAssert(new ClassPathCqlScript("../cql/keyspace.cql", getClass()));
-		Cassandra c = runner.run(cqlAssert.andThen(cassandra -> {
+		Cassandra c = runner.run(assertCreateKeyspace().andThen(cassandra -> {
 			this.output.reset();
 			cassandra.start();
 			assertThat(this.output.toString()).doesNotContain("Starts Apache Cassandra");
@@ -119,64 +124,79 @@ public abstract class AbstractLocalCassandraTests {
 	@Test
 	public void shouldRunMoreThanOneCassandra() {
 		CassandraRunner runner = new CassandraRunner(this.factory);
-		CqlAssert cqlAssert = new CqlAssert(new ClassPathCqlScript("../cql/keyspace.cql", getClass()));
 		runner.run(cassandra -> {
-			cqlAssert.accept(cassandra);
-			runner.run(cqlAssert);
+			assertCreateKeyspace().accept(cassandra);
+			runner.run(assertCreateKeyspace());
 		});
+	}
+
+
+	@Test
+	public void shouldBeRestarted() {
+		this.factory.setConfigurationFile(ClassLoader.getSystemResource("cassandra-static.yaml"));
+		this.factory.getJvmOptions().add("-Dcassandra.jmx.local.port=7199");
+		new CassandraRunner(this.factory)
+				.run(assertCreateKeyspace().andThen(assertPort(7199, 9042)));
+		new CassandraRunner(this.factory)
+				.run(assertCreateKeyspace().andThen(assertPort(7199, 9042)));
+
 	}
 
 	@Test
 	public void shouldKeepDataBetweenLaunches() {
-		this.factory.setConfigurationFile(getClass().getResource("/cassandra-static.yaml"));
 		this.factory.setWorkingDirectory(FileUtils.getUserDirectory()
 				.resolve("target")
 				.resolve(UUID.randomUUID().toString()));
-		this.factory.getJvmOptions().add("-Dcassandra.jmx.local.port=9100");
 		try {
 			CassandraRunner runner = new CassandraRunner(this.factory);
-			OutputAssert outputAssert = new OutputAssert(this.output, "-Dcassandra.jmx.local.port=9100");
-			runner.run(outputAssert.andThen(new CqlAssert(new ClassPathCqlScript("../cql/keyspace.cql", getClass()))));
-			this.output.reset();
-			runner.run(outputAssert.andThen(new CqlAssert(new StaticCqlScript("DROP KEYSPACE test"))));
+			runner.run(assertCreateKeyspace());
+			runner.run(assertDeleteKeyspace());
 		}
 		finally {
 			IOUtils.closeQuietly(() -> FileUtils.delete(this.factory.getWorkingDirectory()));
 		}
 	}
 
-	/**
-	 * Assert {@code Cassandra} output.
-	 */
-	protected static final class OutputAssert implements Consumer<Cassandra> {
 
-		@Nonnull
-		private final CaptureOutput output;
-
-		@Nonnull
-		private final String message;
-
-		OutputAssert(@Nonnull CaptureOutput output, @Nonnull String message) {
-			this.output = output;
-			this.message = message;
-		}
-
-		@Override
-		public void accept(Cassandra cassandra) {
-			assertThat(this.output.toString()).contains(this.message);
-		}
+	private Consumer<Cassandra> assertDeleteKeyspace() {
+		return new CqlAssert(new StaticCqlScript("DROP KEYSPACE test"));
 	}
+
+	private Consumer<Cassandra> assertCreateKeyspace() {
+		return assertScript(new ClassPathCqlScript("../cql/keyspace.cql", getClass()));
+	}
+
+	private Consumer<Cassandra> assertScript(CqlScript... scripts) {
+		return new CqlAssert(scripts);
+	}
+
+	private Consumer<Cassandra> assertPort(Function<Settings, Integer> mapper) {
+		return cassandra -> assertPort(mapper.apply(cassandra.getSettings()));
+	}
+
+	private Consumer<Cassandra> assertPort(int... port) {
+		Consumer<Cassandra> assertPort = new PortAssert(port[0]);
+		for (int i = 1; i < port.length; i++) {
+			assertPort = assertPort.andThen(new PortAssert(port[i]));
+		}
+		return assertPort;
+	}
+
 
 	/**
 	 * Assert {@code CQL} scripts could be invoked.
 	 */
-	protected static final class CqlAssert implements Consumer<Cassandra> {
+	private static final class CqlAssert implements Consumer<Cassandra> {
 
 		@Nonnull
 		private final CqlScript script;
 
 		CqlAssert(@Nullable CqlScript... scripts) {
 			this.script = new CqlScripts(scripts);
+		}
+
+		CqlAssert(@Nullable String statement) {
+			this(StringUtils.hasText(statement) ? new CqlScript[]{new StaticCqlScript(statement)} : new CqlScript[0]);
 		}
 
 		@Override
@@ -203,6 +223,25 @@ public abstract class AbstractLocalCassandraTests {
 			}
 		}
 
+	}
 
+	/**
+	 * Assert {@code port} is busy.
+	 */
+	private static final class PortAssert implements Consumer<Cassandra> {
+
+		private final int port;
+
+		private PortAssert(int port) {
+			this.port = port;
+		}
+
+		@Override
+		public void accept(@Nonnull Cassandra cassandra) {
+			Settings settings = cassandra.getSettings();
+			assertThat(PortUtils.isPortBusy(settings.getAddress(), this.port))
+					.describedAs("Port (%s) is not busy", this.port)
+					.isTrue();
+		}
 	}
 }
