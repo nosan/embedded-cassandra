354a7828bc906ab12874c5a91081f234f81d4902
Dmytro Nosan <dimanosan@gmail.com>
2018-12-25 20:48:28 +0200

    add ThreadNameSupplier and ThreadUtils

---
+++ embedded-cassandra-parent/embedded-cassandra/src/main/java/com/github/nosan/embedded/cassandra/util/ThreadUtils.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2018-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.nosan.embedded.cassandra.util;
+
+import javax.annotation.Nullable;
+
+import org.apiguardian.api.API;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Utility class for dealing with {@link Thread}.
+ *
+ * @author Dmytro Nosan
+ * @since 1.2.8
+ */
+@API(since = "1.2.8", status = API.Status.INTERNAL)
+public abstract class ThreadUtils {
+
+	private static final Logger log = LoggerFactory.getLogger(ThreadUtils.class);
+
+	/**
+	 * Waits for a thread to die.
+	 *
+	 * @param thread thread to join
+	 * @throws InterruptedException if any thread has interrupted the current thread.
+	 */
+	public static void join(@Nullable Thread thread) throws InterruptedException {
+		if (thread != null && thread.isAlive()) {
+			if (log.isDebugEnabled()) {
+				log.debug("{} <join to> {}", Thread.currentThread(), thread);
+			}
+			thread.join();
+		}
+
+	}
+
+	/**
+	 * Interrupts a thread.
+	 *
+	 * @param thread thread to interrupt
+	 * @throws SecurityException – if the current thread cannot modify this thread
+	 */
+	public static void interrupt(@Nullable Thread thread) throws SecurityException {
+		if (thread != null && thread.isAlive() && !thread.isInterrupted()) {
+			if (log.isDebugEnabled()) {
+				log.debug("{} <interrupt> {}", Thread.currentThread(), thread);
+			}
+			thread.interrupt();
+		}
+	}
+
+}
---
+++ embedded-cassandra-parent/embedded-cassandra/src/test/java/com/github/nosan/embedded/cassandra/util/ThreadUtilsTests.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2018-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.nosan.embedded.cassandra.util;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link ThreadUtils}.
+ *
+ * @author Dmytro Nosan
+ */
+public class ThreadUtilsTests {
+
+	private static final Runnable TARGET = () -> {
+		try {
+			Thread.sleep(3000);
+		}
+		catch (InterruptedException ex) {
+			Thread.currentThread().interrupt();
+		}
+	};
+
+	@Test
+	public void join() throws InterruptedException {
+		long millis = System.currentTimeMillis();
+		Thread thread = new Thread(TARGET);
+		thread.start();
+		ThreadUtils.join(thread);
+		long elapsed = System.currentTimeMillis() - millis;
+		assertThat(elapsed).isGreaterThanOrEqualTo(3000);
+
+	}
+
+	@Test
+	public void interrupt() throws InterruptedException {
+		long millis = System.currentTimeMillis();
+		Thread thread = new Thread(TARGET);
+		thread.start();
+		ThreadUtils.interrupt(thread);
+		ThreadUtils.join(thread);
+		long elapsed = System.currentTimeMillis() - millis;
+		assertThat(elapsed).isLessThan(3000);
+	}
+}
