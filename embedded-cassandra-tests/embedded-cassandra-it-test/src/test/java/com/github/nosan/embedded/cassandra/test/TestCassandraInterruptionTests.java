00750d9e8dc34dfebcba308b89ae6c7439f90027
Dmytro Nosan <dimanosan@gmail.com>
2019-01-14 12:23:49 +0200

    add interruption tests

---
+++ embedded-cassandra-tests/embedded-cassandra-it-test/src/test/java/com/github/nosan/embedded/cassandra/test/TestCassandraInterruptionTests.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2018-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.nosan.embedded.cassandra.test;
+
+import java.util.Collection;
+
+import javax.annotation.Nonnull;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import com.github.nosan.embedded.cassandra.cql.CqlScript;
+import com.github.nosan.embedded.cassandra.test.support.CaptureOutput;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link TestCassandra}.
+ *
+ * @author Dmytro Nosan
+ */
+public class TestCassandraInterruptionTests {
+
+	@Rule
+	public final CaptureOutput output = new CaptureOutput();
+
+	@Test
+	public void interruptTestCassandra() throws InterruptedException {
+		TestCassandra cassandra = new TestCassandra(new CqlScript() {
+			@Nonnull
+			@Override
+			public Collection<String> getStatements() {
+				throw new IllegalStateException("Why are you here ?");
+			}
+		});
+
+		Thread thread = new Thread(cassandra::start);
+		thread.start();
+		Thread.sleep(5000);
+		thread.interrupt();
+		thread.join();
+
+		assertThat(this.output.toString()).contains("Test Cassandra launch was interrupted");
+		assertThat(this.output.toString()).contains("Cassandra launch was interrupted");
+		assertThat(this.output.toString()).contains("Apache Cassandra (3.11.3) has been stopped");
+
+	}
+}
