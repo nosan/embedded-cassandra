e98c641e4c5566310a496668ace4558754c5b6a3
Dmytro Nosan <dimanosan@gmail.com>
2019-01-24 13:30:57 +0200

    add spring-boot  +  spring-boot-data-cassandra tests

---
+++ embedded-cassandra-tests/embedded-cassandra-spring-boot-test/src/test/java/com/github/nosan/embedded/cassandra/test/spring/data/City.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2018-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.nosan.embedded.cassandra.test.spring.data;
+
+import com.datastax.driver.core.DataType.Name;
+import org.springframework.data.cassandra.core.mapping.CassandraType;
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+/**
+ * Simple entity to describe the city.
+ *
+ * @author Dmytro Nosan
+ */
+@Table("city")
+public class City {
+
+	@PrimaryKey
+	@CassandraType(type = Name.BIGINT)
+	private Long id;
+
+	@Column
+	private String name;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
