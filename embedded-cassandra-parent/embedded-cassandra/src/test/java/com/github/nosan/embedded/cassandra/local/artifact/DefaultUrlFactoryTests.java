530712459d63635a453bd4d16966c6b09e0f4c26
Dmytro Nosan <dimanosan@gmail.com>
2019-02-17 12:18:24 +0200

    use http://

--- embedded-cassandra-parent/embedded-cassandra/src/test/java/com/github/nosan/embedded/cassandra/local/artifact/DefaultUrlFactoryTests.java
+++ embedded-cassandra-parent/embedded-cassandra/src/test/java/com/github/nosan/embedded/cassandra/local/artifact/DefaultUrlFactoryTests.java
@@ -40,7 +40,7 @@ public class DefaultUrlFactoryTests {
 				.isEqualTo(new URL[]{
 						new URL("https://apache.org/dyn/closer.cgi?action=download&filename=cassandra/3.11.3/" +
 								"apache-cassandra-3.11.3-bin.tar.gz"),
-						new URL("https://archive.apache.org/dist/cassandra/3.11.3/" +
+						new URL("http://archive.apache.org/dist/cassandra/3.11.3/" +
 								"apache-cassandra-3.11.3-bin.tar.gz")});
 
 	}
