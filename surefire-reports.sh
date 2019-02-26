dbaccd95267caf74398c8c746bb231cd8d3c4582
Dmytro Nosan <dimanosan@gmail.com>
2019-02-26 18:40:44 +0200

    adds surefire-reports.sh

---
+++ surefire-reports.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo
+echo "===== SUREFIRE REPORTS ====="
+echo
+for dir in $(find . -type d -name 'surefire-reports'); do
+  for file in $(find "$dir" -type f -name '*.txt'); do
+     if grep -q -w "Errors: [^0]" ${file}; then
+        cat ${file}
+        echo
+        if  [[ -f "${file%.*}-output.txt" ]]; then
+           cat "${file%.*}-output.txt"
+        fi
+        echo
+        echo
+     fi
+  done
+done
+
