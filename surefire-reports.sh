dfb72831ede75bf1b6ea6772768c583953915433
Dmytro Nosan <dimanosan@gmail.com>
2019-02-27 13:26:25 +0200

    polish surefire-reports.sh

--- surefire-reports.sh
+++ surefire-reports.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-echo
-echo "===== SUREFIRE REPORTS ====="
-echo
 for dir in $(find . -type d -name 'surefire-reports'); do
   for file in $(find "$dir" -type f -name '*.txt'); do
      if grep -E "(Failures|Errors): [^0]" ${file}; then
+        echo
         cat ${file}
         echo
         if  [[ -f "${file%.*}-output.txt" ]]; then
            cat "${file%.*}-output.txt"
         fi
-        echo
-        echo
      fi
   done
 done
