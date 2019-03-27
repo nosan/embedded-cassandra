58e854e5eda4a7c58daec9c3aadf536293016ae9
Dmytro Nosan <dimanosan@gmail.com>
2019-03-27 15:32:03 +0200

    update test

--- embedded-cassandra-parent/embedded-cassandra/src/test/java/com/github/nosan/embedded/cassandra/util/FileLockTests.java
+++ embedded-cassandra-parent/embedded-cassandra/src/test/java/com/github/nosan/embedded/cassandra/util/FileLockTests.java
@@ -19,17 +19,20 @@ package com.github.nosan.embedded.cassandra.util;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -40,22 +43,28 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 class FileLockTests {
 
-	private static final Logger log = LoggerFactory.getLogger(FileLockTests.class);
-
 	@Test
 	void shouldLockUsingFile(@TempDir Path temporaryFolder) throws Exception {
 		long start = System.currentTimeMillis();
 		Path fileLock = temporaryFolder.resolve(String.format("%s.lock", UUID.randomUUID()));
 		List<Process> processes = new ArrayList<>();
+		Map<Process, ProcessReader> readers = new LinkedHashMap<>();
 		for (int i = 0; i < 3; i++) {
 			processes.add(fork(fileLock));
 		}
 		for (Process process : processes) {
-			assertThat(process.waitFor()).isZero();
+			readers.put(process, new ProcessReader(process));
+		}
+		for (Process process : processes) {
+			assertThat(process.waitFor())
+					.describedAs(String.format("Process exit code %s is not valid. %n%s", process.exitValue(),
+							readers.get(process))).isZero();
 		}
 		long elapsed = System.currentTimeMillis() - start;
-		assertThat(elapsed).describedAs("Seems like 'FileLock' does not work correctly.").isGreaterThan(1500)
-				.isLessThan(5000);
+		assertThat(elapsed).describedAs("Seems like 'FileLock' does not work correctly.%n%s",
+				readers.entrySet().stream().map(entry -> String.format("[%s] %s", entry.getKey(), entry.getValue()))
+						.collect(Collectors.joining(System.lineSeparator())))
+				.isGreaterThan(1500).isLessThan(5000);
 
 	}
 
@@ -72,16 +81,39 @@ class FileLockTests {
 		builder.command().add(new SystemProperty("java.class.path").getRequired());
 		builder.command().add(FileLockSuite.class.getCanonicalName());
 		builder.command().add(fileLock.toAbsolutePath().toString());
-		Process process = builder.start();
-		new Thread(() -> {
-			try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-				bufferedReader.lines().forEach(log::info);
+
+		return builder.start();
+
+	}
+
+	private static final class ProcessReader extends Thread {
+
+		private final Process process;
+
+		private final StringBuilder output = new StringBuilder();
+
+		ProcessReader(Process process) {
+			this.process = process;
+			start();
+		}
+
+		@Override
+		public void run() {
+			try (BufferedReader bufferedReader = new BufferedReader(
+					new InputStreamReader(this.process.getInputStream()))) {
+				bufferedReader.lines().forEach(line -> this.output.append(line).append(System.lineSeparator()));
 			}
 			catch (IOException ex) {
-				log.error(ex.getMessage(), ex);
+				StringWriter writer = new StringWriter();
+				ex.printStackTrace(new PrintWriter(writer));
+				this.output.append(writer);
 			}
-		}).start();
-		return process;
+		}
+
+		@Override
+		public String toString() {
+			return this.output.toString();
+		}
 
 	}
 
