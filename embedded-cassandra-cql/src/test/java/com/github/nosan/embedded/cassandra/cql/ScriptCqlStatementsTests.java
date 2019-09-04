f316d81cdfd3d03b10396fe974059d7dda92b7ba
Dmytro Nosan <dimanosan@gmail.com>
2019-09-04 15:25:11 +0300

    Add Cql Support module.

---
+++ embedded-cassandra-cql/src/test/java/com/github/nosan/embedded/cassandra/cql/ScriptCqlStatementsTests.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2018-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.nosan.embedded.cassandra.cql;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Tests for {@link ScriptCqlStatements}.
+ *
+ * @author Dmytro Nosan
+ */
+class ScriptCqlStatementsTests {
+
+	private final CqlStatements statements = CqlStatements.forScripts(
+			"CREATE KEYSPACE test WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 };"
+					+ "CREATE TABLE test.roles ( id text PRIMARY KEY )",
+			"CREATE TABLE test.users ( user_id text PRIMARY KEY, first_name text, last_name text, emails set<text> )");
+
+	@Test
+	void testGetStatements() {
+		List<String> statements = this.statements.stream().collect(Collectors.toList());
+		assertThat(statements).contains(
+				"CREATE KEYSPACE test WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }");
+		assertThat(statements).contains("CREATE TABLE test.roles ( id text PRIMARY KEY )");
+		assertThat(statements).contains("CREATE TABLE test.users ( user_id text PRIMARY KEY, first_name text,"
+				+ " last_name text, emails set<text> )");
+	}
+
+}
