dbf15e17879609d4d43c902e7ae91838d32160c6
Dmytro Nosan <dimanosan@gmail.com>
2019-09-12 12:15:37 +0300

    Update mvn.release

--- mvn-release.sh
+++ mvn-release.sh
@@ -2,19 +2,18 @@
 # shellcheck disable=SC2039
 read -r -p "Release Version: " releaseVersion
 read -r -p "Development Version: " developmentVersion
+read -r -p "Release: '${releaseVersion}' and Development: '${developmentVersion}'. Y/N? " answer
 
-echo "Release: ${releaseVersion} and Development: ${developmentVersion}. Y/N"
-
-read -r -p proceed
-
-if [ "${proceed}" != "Y" ]; then
+if [ "${answer}" != "Y" ]; then
   echo "Abort"
   exit 1
 fi
 
 release() {
-  ./mvnw versions:set -DnewVersion="${releaseVersion}" -DgenerateBackupPoms=false && ./mvnw clean verify -Prelease -DskipTests && git reset HEAD --hard && git clean -df &&
-    ./mvnw release:prepare -Prelease -DautoVersionSubmodules=true -Dtag="${releaseVersion}" -DreleaseVersion="${releaseVersion}" -DdevelopmentVersion="${developmentVersion}" &&
+  git reset HEAD --hard && git clean -df &&
+    ./mvnw release:prepare -Prelease -DautoVersionSubmodules=true \
+      -Dtag="${releaseVersion}" -DreleaseVersion="${releaseVersion}" \
+      -DdevelopmentVersion="${developmentVersion}" &&
     ./mvnw release:perform -Prelease
 }
 
