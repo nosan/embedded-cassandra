ddac7997340492179aa6e1999ed7fd389f16e17d
Dmytro Nosan <dimanosan@gmail.com>
2021-01-16 23:24:57 +0200

    Upgrade to Cassandra 4.0-beta4

--- src/main/java/com/github/nosan/embedded/cassandra/CassandraBuilder.java
+++ src/main/java/com/github/nosan/embedded/cassandra/CassandraBuilder.java
@@ -56,7 +56,7 @@ public class CassandraBuilder {
 	/**
 	 * Default Cassandra version.
 	 */
-	public static final Version DEFAULT_VERSION = Version.parse("4.0-beta3");
+	public static final Version DEFAULT_VERSION = Version.parse("4.0-beta4");
 
 	private static final AtomicInteger CASSANDRA_ID = new AtomicInteger();
 
--- src/test/java/com/github/nosan/embedded/cassandra/DefaultCassandraIntegrationTests.java
+++ src/test/java/com/github/nosan/embedded/cassandra/DefaultCassandraIntegrationTests.java
@@ -439,7 +439,7 @@ class DefaultCassandraIntegrationTests {
 	}
 
 	private static Stream<Version> versions() {
-		return Stream.of(Version.parse("4.0-beta3"), Version.parse("3.11.9"));
+		return Stream.of(Version.parse("4.0-beta4"), Version.parse("3.11.9"));
 	}
 
 	private interface CassandraConsumer {
