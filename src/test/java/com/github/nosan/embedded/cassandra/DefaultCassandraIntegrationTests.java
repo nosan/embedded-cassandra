79e1ebdbda2b608cf714c201cb7aee258ffe4a77
Dmytro Nosan <dimanosan@gmail.com>
2022-05-20 11:11:09 +0300

    Update to Apache Cassandra 4.0.4

--- src/test/java/com/github/nosan/embedded/cassandra/DefaultCassandraIntegrationTests.java
+++ src/test/java/com/github/nosan/embedded/cassandra/DefaultCassandraIntegrationTests.java
@@ -357,7 +357,7 @@ class DefaultCassandraIntegrationTests {
 	}
 
 	private static Stream<Version> versions() {
-		return Stream.of(Version.parse("4.0.3"));
+		return Stream.of(Version.parse("4.0.4"));
 	}
 
 	private interface CassandraConsumer {
