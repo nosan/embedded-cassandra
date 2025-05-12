8391cca41f4db70507470214d58c1db9ef4f4a3e
Dmytro Nosan <dimanosan@gmail.com>
2025-05-12 19:53:01 +0300

    Update Default Apache Cassandra Version to 5.0.4

--- src/main/java/com/github/nosan/embedded/cassandra/CassandraBuilder.java
+++ src/main/java/com/github/nosan/embedded/cassandra/CassandraBuilder.java
@@ -58,7 +58,7 @@ public class CassandraBuilder {
 	/**
 	 * Default Cassandra version.
 	 */
-	public static final Version DEFAULT_VERSION = Version.parse("5.0.3");
+	public static final Version DEFAULT_VERSION = Version.parse("5.0.4");
 
 	private static final AtomicInteger CASSANDRA_ID = new AtomicInteger();
 
